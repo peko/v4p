f4dbfebda0724e06684e9e506873a5aee2819e1b
sylvain.garden@gmail.com <sylvain.garden@gmail.com@4ee0ea0d-2345-0410-90fa-e36e85c9abab>
2010-11-24 00:53:03 +0000

    scaling bugs all corrected

--- trunk/demo/square/main.c
+++ trunk/demo/square/main.c
@@ -2,25 +2,42 @@
 #include "v4p.h"
 #include "v4pi.h"
 
+#define STRESS_AMOUNT 24
+
 PolygonP  scene;
 Boolean gmOnInit() {
+  Coord x = 40, y = 40;
   int i;
-  v4pDisplayInit(1, 1);
+  int j;
+  int k;
+  v4pDisplayInit(1, 0);
   v4pInit();
   scene = NULL;
   v4pSetScene(&scene);
+  v4pSetBGColor(blue);
+ 
+  PolygonP pColMatrix[STRESS_AMOUNT][STRESS_AMOUNT];
   PolygonP pCol=v4pPolygonNew(absolute, red, 10);
-  v4pPolygonRect(pCol, -v4pDisplayWidth / 3, -v4pDisplayHeight / 3,
-    v4pDisplayWidth / 3, v4pDisplayHeight / 3);
-  PolygonP pCenteredCol = v4pPolygonClone(pCol);
-  v4pPolygonIntoList(pCenteredCol, &scene);
+  v4pPolygonRect(pCol, -v4pDisplayWidth / 3 + v4pDisplayWidth, -v4pDisplayHeight / 3, v4pDisplayWidth / 3, v4pDisplayHeight / 3);
 
-  for (i = 0; i<500;i++) {
-   v4pSetView(-i, -i, v4pDisplayWidth + i, v4pDisplayHeight + i);
-   v4pPolygonTransformClone(pCol, pCenteredCol, v4pDisplayWidth / 2, v4pDisplayHeight / 2, 45, 0);
-   v4pRender();
+  for (j= 0; j < STRESS_AMOUNT; j++) {
+    for (k = 0; k < STRESS_AMOUNT; k++) {
+      pColMatrix[j][k] = v4pPolygonClone(pCol);
+      v4pPolygonIntoList(pColMatrix[j][k], &scene);
+    }
   }
-  sleep(2);
+
+  for (i = 0 ; i < STRESS_AMOUNT * 300 ; i+= 2 * STRESS_AMOUNT) {
+    v4pSetView(-i, -i, v4pDisplayWidth + i, v4pDisplayHeight + i);
+    for (j= 0; j < STRESS_AMOUNT; j++) {
+      for (k = 0; k < STRESS_AMOUNT; k++) {
+       v4pPolygonTransformClone(pCol, pColMatrix[j][k], v4pDisplayWidth * (j - STRESS_AMOUNT / 2), v4pDisplayHeight * (k - STRESS_AMOUNT / 2), (j * k) + 16 * i / STRESS_AMOUNT, 0);
+      }
+    }
+    v4pRender();
+    //break;
+  }
+  { char buf[2]; fgets(buf, 1, stdin); }
 }
 Boolean gmOnIterate() {
   return failure;
