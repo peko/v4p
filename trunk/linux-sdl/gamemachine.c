49d52ccdd30b99e5456c3a382a127d099c67d92a
sylvain.garden@gmail.com <sylvain.garden@gmail.com@4ee0ea0d-2345-0410-90fa-e36e85c9abab>
2010-12-21 07:53:17 +0000

    gameMachine now working again

--- trunk/linux-sdl/gamemachine.c
+++ trunk/linux-sdl/gamemachine.c
@@ -4,94 +4,106 @@
 #include "v4pi.h"
 #include "SDL/SDL.h"
 
+// The machine states holds basic up-to-date data
 GmState gmMachineState;
-int     gmFramerate = 30;
 
+//framerate stuff
+#define DEFAULT_FRAMERATE 30
+#define MAX_PERIOD (5 * 60000)
+int     gmFramerate = DEFAULT_FRAMERATE;
+static  int gmPeriod = 1000 / DEFAULT_FRAMERATE; // private
+
+// change the framerate
 int gmSetFramerate(int new) {
-	return (gmFramerate = new);
+  gmFramerate = new;
+  gmPeriod = (new > 0 ? 1000 / gmFramerate : MAX_PERIOD);
+	return (new);
 }
 
-Boolean poll() {
-    Boolean exit=0;
-    SDL_Surface *screen;
-    SDL_Event event;
+// poll user events
+Boolean gmPollEvents() {
+  Boolean rc = 0; // return code
 
-      while (SDL_PollEvent(&event)) 
-        { // polling
+  SDL_Event event; // event data
 
-          switch (event.type) 
-            { // event
+  while (!rc && SDL_PollEvent(&event))  { // polling loop
+    switch (event.type) { // event cases
+       case SDL_QUIT: // time to quit
+         rc = 1;
+         break;
 
-            case SDL_QUIT:
-              exit = 1;
-              
-              break;
-            case SDL_KEYDOWN: {
-              SDLKey keypressed = event.key.keysym.sym;
-              if (keypressed == SDLK_ESCAPE) 
-                exit = 1;
-              
-              break;
-            }
-            case SDL_MOUSEBUTTONDOWN:
-              
-              gmMachineState.buttons[0] = 1;
-              break;
+       case SDL_KEYDOWN: {
+         SDLKey keypressed = event.key.keysym.sym;
+         if (keypressed == SDLK_ESCAPE) 
+            rc = 1;
+            break;
+         }
+       case SDL_MOUSEBUTTONDOWN:
+          gmMachineState.buttons[0] = 1;
+          break;
 
-            case SDL_MOUSEBUTTONUP:
-              
-              gmMachineState.buttons[0] = 0;
-              break;
+       case SDL_MOUSEBUTTONUP:
+          gmMachineState.buttons[0] = 0;
+          break;
 
-            case SDL_MOUSEMOTION:
-              gmMachineState.xpen = event.motion.x;
-              gmMachineState.ypen = event.motion.y;
-              break;
-            
-            }
-        }
-   return exit;
+       case SDL_MOUSEMOTION:
+          gmMachineState.xpen = event.motion.x;
+          gmMachineState.ypen = event.motion.y;
+          break;
+       } // switch
+    } // loop
+   return rc;
 }
 
+// Machine main function
 int gmMain(int argc, char* argv[])
 {
-    SDL_Surface *screen;
-    Boolean exit=0;
-    UInt32 excess, beforeTime, overSleepTime, afterTime,
+    Boolean rc = 0;
+    Int32 excess, beforeTime, overSleepTime, afterTime,
        timeDiff, sleepTime;
-    int  period = 1000 / gmFramerate;
-    excess = 0;
+
+    // reset machine state
     gmMachineState.buttons[0] = 0;
 
-    gmOnInit();
+    // Init call-back
+    if (gmOnInit())
+      return failure;
 
     afterTime = SDL_GetTicks();
     sleepTime = 0;
-    while (!exit)  { //main loop
+    excess = 0;
+    while (!rc)  { //main machine loop
+      // w/ clever hackery to handle properly performance drops
       beforeTime = SDL_GetTicks();
       overSleepTime = (beforeTime - afterTime) - sleepTime;
-      exit |= poll();
-      exit |= gmOnIterate();
-      exit |= gmOnFrame();
 
-      period = 1000 / gmFramerate;
+      // poll user events
+      rc |= gmPollEvents();
+      // process scene iteration
+      rc |= gmOnIterate();
+      // render a frame
+      rc |= gmOnFrame();
+
+      // maximize frame rates and detect performance drops
       afterTime = SDL_GetTicks();
       timeDiff = afterTime - beforeTime;
-      sleepTime = (period - timeDiff) - overSleepTime;
+      sleepTime = (gmPeriod - timeDiff) - overSleepTime;
       if (sleepTime <= 0) {
         excess -= sleepTime;
         sleepTime = 2;
       }
       SDL_Delay(sleepTime);
 
-      while (excess > period) {
-        exit |= poll();
-        exit |= gmOnIterate();
-        excess -= period;
+      // when framerate is low, one repeats non-display steps
+      while (excess > gmPeriod) {
+        rc |= gmPollEvents();
+        rc |= gmOnIterate();
+        excess -= gmPeriod;
       }
     }
-    
+
+    // we're done.    
     gmOnQuit();
     
-    return EXIT_SUCCESS;
+    return success;
 }
